bbf5d9d60f4e047c011e38723dc15ecbedda2de5
docfr10 <docfr10@yandex.ru>
2023-02-17 08:45:04 +0300

    Added text to TextFieldScreen

--- app/src/main/java/com/example/lesson_jetpackcompose/screens/TextFieldScreen.kt
+++ app/src/main/java/com/example/lesson_jetpackcompose/screens/TextFieldScreen.kt
@@ -1,9 +1,7 @@
 package com.example.lesson_jetpackcompose.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -23,33 +21,56 @@ fun TextFieldScreen() {
     val message1 = remember { mutableStateOf("") }
     val message2 = remember { mutableStateOf("") }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier.fillMaxSize(0.5f).background(Color.Green)) {
-            TextField(
-                value = message1.value,
-                onValueChange = { message1.value = it },
-                label = { Text(text = "Введите текст") },
-                placeholder = { Text(text = "Вводимый текст") },
-                singleLine = true,
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Text,
-                    capitalization = KeyboardCapitalization.Sentences,
-                    imeAction = ImeAction.Next
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.Top
+    ) {
+        Column(modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(0.5f)) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(0.5f)
+                    .background(Color.Green)
+            ) {
+                TextField(
+                    value = message1.value,
+                    onValueChange = { message1.value = it },
+                    label = { Text(text = "Введите текст") },
+                    placeholder = { Text(text = "Вводимый текст") },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        capitalization = KeyboardCapitalization.Sentences,
+                        imeAction = ImeAction.Next
+                    )
                 )
-            )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(0.5f)
+                    .background(Color.Magenta)
+            ) {
+                OutlinedTextField(
+                    value = message2.value,
+                    onValueChange = { message2.value = it },
+                    singleLine = false,
+                    label = { Text(text = "Введите текст") },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        capitalization = KeyboardCapitalization.Sentences,
+                        imeAction = ImeAction.Done
+                    )
+                )
+            }
         }
-        Box(modifier = Modifier.fillMaxSize(0.5f).background(Color.Magenta)) {
-            OutlinedTextField(
-                value = message2.value,
-                onValueChange = { message2.value = it },
-                singleLine = false,
-                label = { Text(text = "Введите текст") },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Text,
-                    capitalization = KeyboardCapitalization.Sentences,
-                    imeAction = ImeAction.Done
-                )
-            )
+        Column(
+            Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(0.5f)) {
+            Text(text = message1.value)
+            Text(text = message2.value)
         }
     }
 }
